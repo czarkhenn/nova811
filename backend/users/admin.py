746ffed43571af758c8c68bc9180f8c0721d34bb
czarkhenn <czarkhenn@gmail.com>
2025-09-09 02:37:20 +0800

    Added Users in admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """
+    Custom admin configuration for the User model.
+    """
+    
+    # Fields to display in the admin list view
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_staff', 'is_superuser', 'two_factor_enabled', 'created_at')
+    list_filter = ('role', 'is_staff', 'is_superuser', 'two_factor_enabled', 'created_at')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('-created_at',)
+    
+    # Fields to display in the admin detail view
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone_number')}),
+        (_('Permissions'), {
+            'fields': ('role', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Security'), {'fields': ('two_factor_enabled',)}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    # Fields to display when adding a new user
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'role', 'password1', 'password2'),
+        }),
+    )
+    
+    # Use email as the username field
+    username_field = 'email'
